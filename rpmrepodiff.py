efa3cff3bbdad995f705dde031778f7c7229bd44
leehuk <lee@leeh.uk>
2016-11-06 15:34:09 +0000

    - add initial version

---
+++ rpmrepodiff.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+#
+# rpmrepodiff.py
+#	Tool for diffing RPM repositories
+#
+# Copyright (C) 2016, Lee H <lee@leeh.uk>
+# Released under the BSD 2-Clause License
+
+import collections
+import gzip
+import json
+import re
+import StringIO
+import time
+import xml.etree.ElementTree as ET
+
+from urlgrabber import urlread
+
+def parse_repomd(url):
+	xmldata = ET.fromstring(urlread(url))
+	for mdelem in xmldata.iter('{http://linux.duke.edu/metadata/repo}data'):
+		if mdelem.attrib['type'] != 'primary':
+			continue
+
+		for tag in mdelem:
+			if tag.tag == '{http://linux.duke.edu/metadata/repo}location':
+				return tag.attrib['href']
+
+	return None
+
+def parse_primarymd(url):
+	rpmdata = {}
+	primarymdcontent = urlread(url)
+
+	# decompress if necessary
+	if re.search('\.gz$', url):
+		primarymdcontentgz = StringIO.StringIO(primarymdcontent)
+		with gzip.GzipFile(fileobj=primarymdcontentgz) as f:
+			primarymdcontent = f.read()
+
+	xmldata = ET.fromstring(primarymdcontent)
+	for packagedata in xmldata.iter('{http://linux.duke.edu/metadata/common}package'):
+		if packagedata.attrib['type'] != 'rpm':
+			continue
+
+		tagdata = {}
+		for tag in packagedata:
+			if tag.tag == '{http://linux.duke.edu/metadata/common}name':
+				tagdata['name'] = tag.text
+			elif tag.tag == '{http://linux.duke.edu/metadata/common}version':
+				tagdata['version'] = tag.attrib['ver'] + '-' + tag.attrib['rel']
+			elif tag.tag == '{http://linux.duke.edu/metadata/common}arch':
+				tagdata['arch'] = tag.text
+
+		if not tagdata['name'] in rpmdata:
+			rpmdata[tagdata['name']] = []
+
+		rpmdata[tagdata['name']].append(tagdata['version'] + '.' + tagdata['arch'])
+
+	return rpmdata
+
+def rpmdiff_set(rpmdiff, name, mode, version):
+	if not name in rpmdiff:
+		rpmdiff[name] = {}
+
+	if not mode in rpmdiff[name]:
+		rpmdiff[name][mode] = []
+
+	rpmdiff[name][mode].append(version)
+
+#baseurl_src = 'http://repo.loc.lhsys.uk/upstream/centos/7/x86_64/updates/'
+#baseurl_dst = 'http://repo.loc.lhsys.uk/stable/centos/7/x86_64/updates/'
+baseurl_src = 'http://repo.loc.lhsys.uk/tmp/7.0-updates/'
+baseurl_dst = 'http://repo.loc.lhsys.uk/tmp/7.1-updates/'
+
+repomdurl_src = baseurl_src + 'repodata/repomd.xml'
+repomdurl_dst = baseurl_dst + 'repodata/repomd.xml'
+
+primarymd_src_info = parse_repomd(repomdurl_src)
+primarymd_dst_info = parse_repomd(repomdurl_dst)
+
+primarymd_src = baseurl_src + parse_repomd(repomdurl_src)
+primarymd_dst = baseurl_dst + parse_repomd(repomdurl_dst)
+
+rpmdiff = {}
+
+rpmdata_src = parse_primarymd(primarymd_src)
+rpmdata_dst = parse_primarymd(primarymd_dst)
+
+for name, versions in rpmdata_src.items():
+	if name in rpmdata_dst:
+		for version in versions:
+			if not version in rpmdata_dst[name]:
+				rpmdiff_set(rpmdiff, name, 'upgraded_src', version)
+		for version in rpmdata_dst[name]:
+			if not version in rpmdata_src[name]:
+				rpmdiff_set(rpmdiff, name, 'upgraded_dst', version)
+	# package was removed
+	else:
+		for version in versions:
+			rpmdiff_set(rpmdiff, name, 'removed', version)
+
+for name, versions in rpmdata_dst.items():
+	if not name in rpmdata_src:
+		for version in versions:
+			rpmdiff_set(rpmdiff, name, 'added', version)
+
+print json.dumps(rpmdiff)
+time.sleep(60)
